a9fdc48d6e93da28f5021dc5af3ee8f43ab7c501
Weldys Santos <weldyss@gmail.com>
2019-07-18 22:22:34 -0300

    adding bundle config

--- install.sh
+++ install.sh
@@ -5,10 +5,11 @@ DOTPATH=`pwd`
 # Preparing
 
 mkdir -p ~/.ssh
-
+mkdir -p ~/.bundle
 
 # Executing commands
 
+ln -fs $DOTPATH/bundle_config ~/.bundle/config
 ln -fs $DOTPATH/gitconfig ~/.gitconfig
 ln -fs $DOTPATH/gitignore ~/.gitignore
 ln -fs $DOTPATH/irbrc ~/.irbrc
